00c1e7272335ba83777d1e1b7d464156b19c5a2a
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-10-23 16:02:26 +0900

    .envファイルを利用して、環境変数を読み込み

--- twitter-api.js
+++ twitter-api.js
@@ -1,10 +1,12 @@
 import Twitter from "twitter";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const client = new Twitter({
-  consumer_key: "",
-  consumer_secret: "",
-  access_token_key: "",
-  access_token_secret: "",
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
 // let user_list = ["matsu_bouzu", "ariyoshihiroiki"];
